9f34209083f7326b14c10d9aafae8b4128d1af6c
SkylerPIlot <SkylerParthemore@gmail.com>
2021-06-08 17:52:45 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Example
-An example greeter plugin
+# Barbarian Assault Personal Bests
+This Plugin serves to replicate the function of the normal barbarian assault plugin function as well as adding the ability 
+to use the following commands.
+!pb barbarian assault
+!pb ba healer
+!pb ba collector
+!pb ba defender
+!pb ba attacker
+
+The time saved into each one is based on the time shown after w10 is completed and the ability to be saved into a role specific pb can be
+turned on and off though the default is on.
+
+
+If you find any issues with the plugin please submit an issue in this repository and I will try to look at it an fix it as soon as I can.
---
+++ src/main/java/com/BaPB/BaPBConfig.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2018, Cameron <https://github.com/noremac201>
+ * Copyright (c) 2018, Jacob M <https://github.com/jacoblairm>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.BaPB;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("BaPb")
+public interface BaPBConfig extends Config
+{
+	@ConfigItem(
+		keyName = "showTimer",
+		name = "Show call change timer",
+		description = "Show time to next call change"
+	)
+	default boolean showTimer()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "showHealerBars",
+		name = "Show health bars for teammates when healer",
+		description = "Displays team health for healer"
+	)
+	default boolean showHealerBars()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "waveTimes",
+		name = "Show wave and game duration",
+		description = "Displays wave and game duration"
+	)
+	default boolean waveTimes()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "baPB",
+		name = "Save role PB different then Overall PB",
+		description = "Turning this on will save your round time into a specific role round time as well as into the overall pb"
+	)
+	default boolean Seperate()
+	{
+		return true;
+	}
+}
