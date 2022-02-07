94fec5db2079bf73698b76ddda4f7138dc9640ed
SkylerPIlot <SkylerParthemore@gmail.com>
2022-02-07 09:42:29 -0800

    Prevent people from submitting data while being logged out/before gc is set

--- src/main/java/com/BaPB/BaPBPlugin.java
+++ src/main/java/com/BaPB/BaPBPlugin.java
@@ -37,10 +37,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import javax.inject.Inject;
 import lombok.AccessLevel;
 import lombok.Getter;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.MessageNode;
-import net.runelite.api.Varbits;
+import net.runelite.api.*;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.WidgetClosed;
@@ -242,8 +239,9 @@ public class BaPBPlugin extends Plugin
 	@Subscribe
 	public void onConfigChanged(ConfigChanged event) throws IOException {
 		if(config.SubmitPbs()){
-
-			submit_pb(gc);
+			if(gc > 0 && client.getGameState() == GameState.LOGGED_IN) {
+				submit_pb(gc);
+			}
 		}
 	}
 
